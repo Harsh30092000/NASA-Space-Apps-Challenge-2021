a55fb3fe334b6b4ad70ecf60b15fc10f31fab12f
Manoah-3401 <71387514+Manoah-3401@users.noreply.github.com>
2021-10-03 19:51:08 +0530

    Update GetData.js

--- Frontend - ReactJS/src/views/GetData.js
+++ Frontend - ReactJS/src/views/GetData.js
@@ -126,7 +126,7 @@ export class GetData extends Component {
                         {this.state.lat && this.state.lng ?
                             <div className="map">
                                 <img src={`https://maps.googleapis.com/maps/api/staticmap?center=
-          ${this.state.lat},${this.state.lng}&zoom=17&size=1204x500&sensor=false&markers=color:red%7C${this.state.lat},${this.state.lng}&key=API_Key`} alt="map" className="getData__map" />
+          ${this.state.lat},${this.state.lng}&zoom=17&size=1204x500&sensor=false&markers=color:red%7C${this.state.lat},${this.state.lng}&key=API_KEY`} alt="map" className="getData__map" />
                             </div>
                             :
                             <div className="map">
